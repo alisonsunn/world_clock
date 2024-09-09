22cce96fdae531c35feb127ac24f6f76d84bf604
alisonsunn <alison.jyue.sun@gmail.com>
2024-09-09 12:22:18 +1000

    time component added

--- src/component/Time.jsx
+++ src/component/Time.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const StyleTime = styled.div`
+  color: ${({$light, theme}) => $light ? theme.color.light : theme.color.dark }
+`
 
 function Time(props) {
-  const {children} = props;
+  const {light,children} = props;
   return (
-    <div>{children}</div>
+    <StyleTime $light={light}>{children}</StyleTime>
   )
 }
 
